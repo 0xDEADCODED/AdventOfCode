a707b761e4f946c51af4db63f1282d28ca351437
Eole Lake <eolelake@gmail.com>
2024-12-01 14:05:13 -0500

    Less lines

--- 2024/1/sol.py
+++ 2024/1/sol.py
@@ -1,5 +1,2 @@
-import sys
-
-l1,l2 = list(map(sorted, map(list,zip(*[[int(z) for z in y] for y in [x.strip('\n').split() for x in open(sys.argv[1]).readlines()]]))))
-p1,p2 = sum(list(map(lambda x,y: abs(x-y), l1, l2))), sum(list(map(lambda x,y: x*y, l1, [l2.count(z) for z in l1])))
-print(f"{p1=} || {p2=}")
+l1,l2 = list(map(sorted, map(list,zip(*[[int(z) for z in y] for y in [x.strip('\n').split() for x in open('in').readlines()]]))))
+print(f"p1={sum(list(map(lambda x,y: abs(x-y), l1, l2)))} || p2={sum(list(map(lambda x,y: x*y, l1, [l2.count(z) for z in l1])))}")
