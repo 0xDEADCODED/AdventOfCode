37d32b6bfd0787b7daf614faa88c90e690cefa24
Eole Lake <eolelake@gmail.com>
2024-12-16 14:13:48 -0500

    day 15, working p1 and p2

--- 2024/15/sol.py
+++ 2024/15/sol.py
@@ -1,4 +1,4 @@
-data = open('ex3').read().strip().split('\n\n')
+data = open('in').read().strip().split('\n\n')
 grid,moves = [list(x) for x in data[0].split('\n')],data[1].replace('\n','')
 R,C = len(grid),len(grid[0])
 convert = {'^':(-1,0), 'v':(1,0), '<':(0,-1), '>':(0,1)}
@@ -24,8 +24,8 @@ def generate_p2_grid():
     return g2
 
 
-def calc_gps(grid,search):
-    return sum([100*r+c for r in range(R) for c in range(C) if grid[r][c] == search])
+def calc_gps(grid,search,clen):
+    return sum([100*r+c for r in range(R) for c in range(clen) if grid[r][c] == search])
 
 def move_item(grid,r,c,dr,dc):
         grid[r+dr][c+dc] = grid[r][c] 
@@ -35,75 +35,54 @@ def move_item(grid,r,c,dr,dc):
 def move_p1(r,c,dir):
     dr,dc = convert[dir]
     
-    if grid[r+dr][c+dc] == '#':
-        return r,c 
+    if grid[r+dr][c+dc] == '#': return r,c 
     
-    if grid[r+dr][c+dc] == '.':
-        return move_item(grid,r,c,dr,dc)
+    if grid[r+dr][c+dc] == '.': return move_item(grid,r,c,dr,dc)
     
-    if move_p1(r+dr,c+dc,dir) == (r+dr,c+dc):
-        return r,c
+    if move_p1(r+dr,c+dc,dir) == (r+dr,c+dc): return r,c
 
     return move_item(grid,r,c,dr,dc)
 
-to_move = []
-def move_p2(grid,r,c,dir):
-    global to_move
+def move_p2(grid,r,c,dir,to_move):
     dr,dc = convert[dir]
     
-    if grid[r+dr][c+dc] == '#':
-        return r,c
+    if grid[r+dr][c+dc] == '#': return r,c
     
-    if grid[r+dr][c+dc] == '.':
-        to_move.append((r,c,r+dr,c+dc))
-    else:
-        if dir in ['<','>']:
-            if grid[r+dr][c+dc] in ['[',']']:
-                if move_p2(grid,r+dr,c+dc,dir) == (r,c):
-                    return r,c
-                to_move.append((r,c,r+dr,c+dc))
-        elif dir in ['^','v']:
-            if grid[r+dr][c+dc] == '[':
-                lr,lc = move_p2(grid,r+dr,r+dc,dir)
-                rr,rc = move_p2(grid,r+dr,r+dc+1,dir)
-                if (lr,lc) == (r+dr,c+dc) or (rr,rc) == (r+dr,c+dc+1):
-                    return r,c
-                to_move.append((r,c,r+dr,c+dc))
-                to_move.append((r+dr,c+dc,lr,lc))
-                to_move.append((r+dr,c+dc+1,rr,rc))
-            elif grid[r+dr][c+dc] == ']':
-                rr,rc = move_p2(grid,r+dr,r+dc,dir)
-                lr,lc = move_p2(grid,r+dr,r+dc-1,dir)
-                if (rr,rc) == (r+dr,c+dc) or (lr,lc) == (r+dr,c+dc-1):
-                    return r,c
-                to_move.append((r,c,r+dr,c+dc))
-                to_move.append((r+dr,c+dc,rr,rc))
-                print(lr,lc)
-                to_move.append((r+dr,c+dc-1,lr,lc))
-
-
-    if grid[r][c] == '@':
-        print(to_move)
-        for (sr,sc,tr,tc) in to_move:
-            grid[tr][tc] = grid[sr][sc]
-            grid[sr][sc] = '.'
-
-    return r+dr,c+dc
-        
-
+    if dir in ['<','>']:
+        if grid[r+dr][c+dc] in ['[',']']:
+            if move_p2(grid,r,c+dc,dir,to_move) == (r,c+dc):
+                return (r,c)
+            return move_item(grid,r,c,dr,dc)
+        return move_item(grid,r,c,dr,dc)
+    elif dir in ['^','v']:
+        if dir == '^':
+            rbs,lbs = 1,-1
+        else:
+            rbs,lbs = 1,-1
+        if grid[r+dr][c+dc] == '[':
+            center_r,center_c = move_p2(grid,r+dr,c+dc,dir,to_move)
+            close_r,close_c = move_p2(grid,r+dr,c+dc+rbs,dir,to_move)
+            if (close_r,close_c) == (r+dr,c+rbs) or (center_r,center_c) == (r+dr,c+dc):
+                return (r,c)
+        elif grid[r+dr][c+dc] == ']':
+            center_r,center_c = move_p2(grid,r+dr,c+dc,dir,to_move)
+            close_r,close_c = move_p2(grid,r+dr,c+dc+lbs,dir,to_move)
+            if (close_r,close_c) == (r+dr,c+lbs) or (center_r,center_c) == (r+dr,c+dc):
+                return (r,c)
+    
+        to_move.append((r,c))
+        if grid[r][c] == '@':
+            for rr,cc in sorted(set(to_move)) if dir == '^' else set(to_move):
+                move_item(grid,rr,cc,dr,dc)
+        return r+dr,c+dc
 
 def calc_p2(grid):
-    global moves,to_move
-    print_grid(grid)
+    global moves
     sr,sc = [(r,c) for r in range(R) for c in range(C*2) if grid[r][c] == '@'][0]
     for d in moves:
-        to_move = []
-        sr,sc = move_p2(grid,sr,sc,d)
-        print(d)
-        print_grid(grid)
+        sr,sc = move_p2(grid,sr,sc,d,[])
 
-
-    return calc_gps(grid,'[')
+    return calc_gps(grid,'[',C*2)
 
 def calc_p1(grid):
     global moves
@@ -111,9 +90,11 @@ def calc_p1(grid):
     for d in moves:
         sr,sc = move_p1(sr,sc,d)
 
-    return calc_gps(grid,'O')
+    return calc_gps(grid,'O',C)
 
 
 p2_grid = generate_p2_grid()
 p1 = calc_p1(grid)
 p2 = calc_p2(p2_grid)
+
+print(f"{p1=} || {p2=}")
